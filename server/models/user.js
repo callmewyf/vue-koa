9cb443c6d5969c6c97df4e18cd44121eadf263bb
王亚峰 <16609312464@163.com>
2020-12-28 17:28:36 +0800

    修复sequelize.import问题

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,14 @@
+const { DataTypes } = require('sequelize');
 const db = require('../config/db'),
       userModel = '../schema/user';   // 引入表结构
 const TodolistDb = db.Todolist;   // 引入数据库
 
 Object.keys(TodolistDb).forEach(i=>{console.log(i)})
 
-const User = TodolistDb.import(userModel);   // 用sequelize的import方法引入表结构，实例化了User
+// 这里使用sequelize.import会报错，应该是sequelize版本的问题，可回退至5.x.x版本或使用下边的方法
+// const User = TodolistDb.import(userModel);   // 用sequelize的import方法引入表结构，实例化了User
+
+const User = require(userModel)(TodolistDb, DataTypes);
 
 const getUserById = async function (id) {
   const userInfo = await User.findOne({
