f1cc66c5afad2e435f89b32832563ccb093fba7d
王亚峰 <16609312464@163.com>
2020-12-29 16:49:23 +0800

    修改

--- app.js
+++ app.js
@@ -12,20 +12,20 @@ app.use(json());
 app.use(logger());
 
 // 显示执行的时间
-app.use(async (next) => {
+app.use(async (ctx, next) => {
     let start = new Date;
     await next();
     let ms = new Date - start;
     console.log('%s %s - %s', this.method, this.url, ms);
 })
 
-app.on('error', function (err) {
+app.on('error', function (ctx, err) {
     console.log('server error', err);
 })
 
 router.use('/auth', auth.routes());   // 挂载到koa-router上，同时会让所有的auth的请求路径前面加上'/auth'的请求路径
 
-app.use(router.routes());   // 将路由规则挂载到Koa上
+app.use(router.routes());
 
 app.listen(8889, () => {
     console.log('Koa is listening in 8889');
--- server/models/user.js
+++ server/models/user.js
@@ -3,8 +3,6 @@ const db = require('../config/db'),
       userModel = '../schema/user';   // 引入表结构
 const TodolistDb = db.Todolist;   // 引入数据库
 
-Object.keys(TodolistDb).forEach(i=>{console.log(i)})
-
 // 这里使用sequelize.import会报错，应该是sequelize版本的问题，可回退至5.x.x版本或使用下边的方法
 // const User = TodolistDb.import(userModel);   // 用sequelize的import方法引入表结构，实例化了User
 
